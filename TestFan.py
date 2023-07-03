93428367597e69d65a26ece8dc006f1b43dc54d3
unknown <templanzatintin@gmail.com>
2023-07-04 03:14:20 +0800

    pseudocode

--- TestFan.py
+++ TestFan.py
@@ -14,12 +14,14 @@ print(pyfiglet.figlet_format("FAN", font="isometric2", justify="center"))
 
 from ClassFan import Fan
 
+# First Fan properties: maximum speed, radius 10, color yellow, and turn it on
 fanOne = Fan()
 fanOne.set_speed(Fan.FAST)
 fanOne.set_radius(10)
 fanOne.set_color("\033[33;1;3mYellow\033[0m")
 fanOne.set_on(True)
 
+# First Fan print output
 print("~" * 80)
 print("\n")
 print(pyfiglet.figlet_format("FIRST FAN", font="bubble", justify="center"))
@@ -28,12 +30,14 @@ print("\033[32;1;3mRadius:\033[0m", fanOne.get_radius())
 print("\033[32;1;3mColor:\033[0m", fanOne.get_color())
 print("\033[32;1;3mOn:\033[0m", fanOne.get_on())
 
+# Second Fan properties: medium speed, radius 5, color blue, and turn it off
 fanTwo = Fan()
 fanTwo.set_speed(Fan.MEDIUM)
 fanTwo.set_radius(5)
 fanTwo.set_color("\033[34;1;3mBlue\033[0m")
 fanTwo.set_on(False)
 
+# Second Fan print output
 print("\n")
 print("~" * 80)
 print("\n")
