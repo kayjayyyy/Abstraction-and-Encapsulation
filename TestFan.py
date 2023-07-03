c7ea9c2c0c4f66860a92c11198e40ce28a2a021e
unknown <templanzatintin@gmail.com>
2023-07-04 01:26:36 +0800

    Greeting

--- TestFan.py
+++ TestFan.py
@@ -1,3 +1,13 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Assignment no. 9 - Abstraction and Encapsulation
+
+import pyfiglet
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(90))
+print("")
+print(pyfiglet.figlet_format("FAN", font="isometric2", justify="center"))
