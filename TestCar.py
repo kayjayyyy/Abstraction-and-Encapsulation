82c79536a4e0ee25b5dd6e869ba10efd606c7a46
unknown <templanzatintin@gmail.com>
2023-07-05 00:48:04 +0800

    ask for users name

--- TestCar.py
+++ TestCar.py
@@ -11,3 +11,9 @@ print("")
 print("\033[45m ♥ Welcome! ♥ \033[0m".center(90))
 print("")
 print(pyfiglet.figlet_format("CAR INFO'S", font="bulbhead", justify="center"))
+
+name = input("\n\033[033mGood day! What is your name? \033[0m")
+print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
+print("")
+
+print("\033[36m Let's get started! \033[0m".center(90, "~"))
