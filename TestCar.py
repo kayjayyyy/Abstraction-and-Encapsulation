069d33b211e2bf23f50c85bb03dfbf2e89da4db8
unknown <templanzatintin@gmail.com>
2023-07-05 00:44:56 +0800

    greetings

--- TestCar.py
+++ TestCar.py
@@ -1,3 +1,13 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Assignment no. 9 - Abstraction and Encapsulation (Car Class)
+
+import pyfiglet
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(90))
+print("")
+print(pyfiglet.figlet_format("CAR INFO's", font="bulbhead", justify="center"))
