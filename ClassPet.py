90e709990fb34dd993c6ad30f532c5502cd5214c
unknown <templanzatintin@gmail.com>
2023-07-06 01:01:41 +0800

    pseudocode

--- ClassPet.py
+++ ClassPet.py
@@ -2,12 +2,14 @@
 # BSCPE 1-4
 # Assignment no. 9 - Abstraction and Encapsulation (Pet Class)
 
+# Private constructor for pet class with following attributes: name, animal type, and age
 class Pet:
     def __init__(self, name, animal_type, age):
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
-        
+    
+    # The accessor (getters) methods: name, animal type, and age
     def get_name(self):
         return self.__name
     
@@ -17,6 +19,7 @@ class Pet:
     def get_age(self):
         return self.__age
     
+    # The mutator (setters) methods: name, animal type, and age
     def set_name(self, name):
         self.__name = name
 
