10c0996a02cc615640f886e24fcc3e32d232fad3
unknown <templanzatintin@gmail.com>
2023-07-06 00:59:14 +0800

    accessor or getter methods

--- ClassPet.py
+++ ClassPet.py
@@ -8,6 +8,15 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
         
+    def get_name(self):
+        return self.__name
+    
+    def get_animal_type(self):
+        return self.__animal_type
+    
+    def get_age(self):
+        return self.__age
+    
     def set_name(self, name):
         self.__name = name
 
