bbce8c454a60f19dcae8530b56e85e50c8cdd0b6
unknown <templanzatintin@gmail.com>
2023-07-06 00:53:52 +0800

    create a class pet

--- ClassPet.py
+++ ClassPet.py
@@ -1,3 +1,9 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Assignment no. 9 - Abstraction and Encapsulation (Pet Class)
+
+class Pet:
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
