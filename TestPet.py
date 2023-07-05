cd20d306ae9022809482616c6e59a91a7a6d6cdb
unknown <templanzatintin@gmail.com>
2023-07-06 02:03:26 +0800

    ask for pets information

--- TestPet.py
+++ TestPet.py
@@ -13,15 +13,15 @@ print("")
 
 from ClassPet import Pet
 
-your_pet = Pet
-
-print(pyfiglet.figlet_format("Pet's Info", font="bulbhead", justify="center"))
-
-pet_name = input("\033[36;3mEnter the name of your pet: \033[0m")
-pet_type = input("\033[36;3mEnter the type of your pet: \033[0m")
-pet_age = input("\033[36;3mEnter the age of your pet: \033[0m")
+print("")
+print("\033[3m--- Please enter your pet's info ---\033[0m".center(85))
+print("\n")
+pet = Pet (input("\033[36;3mEnter the name of your pet: \033[0m"), input("\033[36;3mEnter the type of your pet: \033[0m"), input("\033[36;3mEnter the age of your pet: \033[0m"))
 
 print("")
 print("-" * 80)
 print("\033[1;3;32mProcessing..........\033[0m".center(90))
 print("-" * 80)
+
+print("\n")
+print(pyfiglet.figlet_format("Pet's Info", font="bulbhead", justify="center"))
