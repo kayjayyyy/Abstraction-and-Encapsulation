24cb306dd574a0c43c53b8be99b77c83f4fb67b3
unknown <templanzatintin@gmail.com>
2023-07-06 01:09:39 +0800

    greeting

--- TestPet.py
+++ TestPet.py
@@ -1,3 +1,12 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Assignment no. 9 - Abstraction and Encapsulation (Pet Class)
+
+import pyfiglet
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(90))
+print("")
